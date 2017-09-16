97bc30f2d1e3f3aabdc552f43b24d064c8f3c553
Alireza Alidousti <alireza.eva.u23@gmail.com>
2017-09-16 07:37:12 -0400

    adds color cube landmarks

--- ARPlay/CoordinateSystemViewController.swift
+++ ARPlay/CoordinateSystemViewController.swift
@@ -10,19 +10,23 @@ import UIKit
 import ARKit
 import SceneKit
 
-class CoordinateSystemViewController: UIViewController, ARSCNViewDelegate, ARSessionObserver {
+class CoordinateSystemViewController: UIViewController, ARSCNViewDelegate {
     let origin = CoordinateSystem() // Model
     
     @IBOutlet weak var sceneView: ARSCNView! {
         didSet {
-            let handler = #selector(CoordinateSystemViewController.moveObject(byReactingTo:))
-            let panRecognizer = UIPanGestureRecognizer(target: self, action: handler)
+            let panHandler = #selector(CoordinateSystemViewController.moveObject(byReactingTo:))
+            let panRecognizer = UIPanGestureRecognizer(target: self, action: panHandler)
             sceneView.addGestureRecognizer(panRecognizer)
+            
+            let tapHandler = #selector(CoordinateSystemViewController.addAllMarkers(byReactingTo:))
+            let tapRecognizer = UITapGestureRecognizer(target: self, action: tapHandler)
+            sceneView.addGestureRecognizer(tapRecognizer)
         }
     }
     
-    private var activeMarker: SCNNode?
-    private var activeMarkerWrapper: SCNNode?
+    var activeMarker: SCNNode?
+    var activeMarkerWrapper: SCNNode?
     @objc func moveObject(byReactingTo panRecognizer: UIPanGestureRecognizer){
         let location = panRecognizer.location(in: sceneView)
         switch panRecognizer.state {
@@ -30,19 +34,34 @@ class CoordinateSystemViewController: UIViewController, ARSCNViewDelegate, ARSes
             if let node = getARObject(at: location){
                 activeMarker = node; break
             }
-            if let anchor = getNewAnchor(at: location){
-                sceneView.session.add(anchor: anchor)
-                activeMarker = cubeNode()
-                activeMarkerWrapper?.addChildNode(activeMarker!)
-            }
         case .changed, .ended:
             if let anchor = getNewAnchor(at: location){
                 sceneView.session.add(anchor: anchor)
                 activeMarker?.removeFromParentNode()
-                activeMarkerWrapper?.addChildNode(activeMarker!)
+                if (activeMarker != nil) { activeMarkerWrapper?.addChildNode(activeMarker!) }
             }
         default:
             break
+        }
+    }
+    
+    let colors: Dictionary<String, UIColor> = [
+        "Red": UIColor.red, "Green": UIColor.green, "Blue": UIColor.blue, "Yellow": UIColor.yellow
+    ]
+    
+    var markersHaveBeenAdded: Bool = false
+    @objc func addAllMarkers(byReactingTo tapRecognizer: UITapGestureRecognizer) {
+        let location = tapRecognizer.location(in: sceneView)
+        if let anchor = getNewAnchor(at: location){
+            sceneView.session.add(anchor: anchor)
+            
+            if (!markersHaveBeenAdded && activeMarkerWrapper != nil){
+                for color in colors.values {
+                    let cube = makeCube(0.03, color: color)
+                    activeMarkerWrapper!.addChildNode(cube)
+                }
+                markersHaveBeenAdded = true
+            }
         }
     }
     
@@ -51,7 +70,7 @@ class CoordinateSystemViewController: UIViewController, ARSCNViewDelegate, ARSes
         activeMarkerWrapper = node
     }
     
-    private func getARObject(at location: CGPoint)->SCNNode? {
+    func getARObject(at location: CGPoint)->SCNNode? {
         // If we hit an AR object
         if let hit = sceneView.hitTest(location, options: nil).first {
             return hit.node
@@ -59,7 +78,7 @@ class CoordinateSystemViewController: UIViewController, ARSCNViewDelegate, ARSes
         return nil
     }
     
-    private func getNewAnchor(at location: CGPoint)->ARAnchor? {
+    func getNewAnchor(at location: CGPoint)->ARAnchor? {
         // Not hitting an AR object
         if let hit = sceneView.hitTest(location, types: .featurePoint).first {
             return ARAnchor(transform: hit.worldTransform)
@@ -67,11 +86,11 @@ class CoordinateSystemViewController: UIViewController, ARSCNViewDelegate, ARSes
         return nil
     }
     
-    func cubeNode(_ scale: CGFloat = 0.03)->SCNNode{
+    func makeCube(_ scale: CGFloat, color: UIColor)->SCNNode{
         let cubeGeometry = SCNBox(width: scale, height: scale, length: scale, chamferRadius: 0.0)
         
         let greenMaterial = SCNMaterial()
-        greenMaterial.diffuse.contents = UIColor.green.withAlphaComponent(0.3)
+        greenMaterial.diffuse.contents = color
         cubeGeometry.materials = [greenMaterial]
         
         return SCNNode(geometry: cubeGeometry)
