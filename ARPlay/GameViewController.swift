7d5bd0e5d8ef2ff7b3904065cae6c5afe1c1ef70
yasster <yassine@v1020-wn-130-40.campus-dynamic.uwaterloo.ca>
2017-09-16 11:00:02 -0700

    Fixes on the View Controller

--- ARPlay/GameViewController.swift
+++ ARPlay/GameViewController.swift
@@ -10,15 +10,16 @@ import UIKit
 import SceneKit
 import ARKit
 
+
 class GameViewController: UIViewController,ARSCNViewDelegate {
-    let game = Game()
+    let game = Game() // Model
+    
     @IBOutlet weak var sceneView: ARSCNView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         sceneView.delegate = self
         sceneView.scene = SCNScene()
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,63 +36,49 @@ class GameViewController: UIViewController,ARSCNViewDelegate {
     
     override func viewDidAppear(_ animated: Bool) {
         super .viewDidAppear(animated)
-        mapSpace()
+        addGrid();
     }
     
-    private func blub(arr: [BrainDead])->[SCNNode]{
-        return arr.enumerated().map {(z, col) in
-            let cube: SCNNode
-            switch col{
-            case .Blue :
-                cube =  cubeNode(0.03, color: UIColor.blue.withAlphaComponent(0.3) )
-            case .Red :
-                cube = cubeNode(0.03, color: UIColor.red.withAlphaComponent(0.3) )
-            case .Green :
-                cube = cubeNode(0.03, color: UIColor.green.withAlphaComponent(0.3) )
-            case .Yellow :
-                cube = cubeNode(0.03, color: UIColor.yellow.withAlphaComponent(0.3) )
-            }
-            return cube
-        }
-    }
+//    private func mapSpace(){
+//        var store: [SCNNode] = []
+//        for (x, slice) in game.space.enumerated() {
+//            for (y, axis) in slice.enumerated() {
+//                for (z, color) in axis.enumerated(){
+//                    let cube: SCNNode
+//                    switch color{
+//                    case .Blue :
+//                        cube =  cubeNode(0.03, color: UIColor.blue)
+//                    case .Red :
+//                        cube = cubeNode(0.03, color: UIColor.red)
+//                    case .Green :
+//                        cube = cubeNode(0.03, color: UIColor.green)
+//                    case .Yellow :
+//                        cube = cubeNode(0.03, color: UIColor.yellow)
+//                    }
+//                    cube.position = SCNVector3(cubeLength * Double(x), cubeLength * Double(y), cubeLength * Double(z))
+//                    store.append(cube)
+//                }
+//            }
+//        }
+//        for node in store {
+//            sceneView.scene.rootNode.addChildNode(node)
+//        }
+//    }
     
-    private func mapSpace(){
-        var store: [SCNNode] = []
-        for (x, slice) in game.space.enumerated() {
-            for (y, axis) in slice.enumerated() {
-                for (z, color) in axis.enumerated(){
-                    let cube: SCNNode
-                    switch color{
-                    case .Blue :
-                        cube =  cubeNode(0.03, color: UIColor.blue.withAlphaComponent(0.3) )
-                    case .Red :
-                        cube = cubeNode(0.03, color: UIColor.red.withAlphaComponent(0.3) )
-                    case .Green :
-                        cube = cubeNode(0.03, color: UIColor.green.withAlphaComponent(0.3) )
-                    case .Yellow :
-                        cube = cubeNode(0.03, color: UIColor.yellow.withAlphaComponent(0.3) )
-                    }
-                    cube.position = SCNVector3(0.03 * Double(x), 0.03 * Double(y), 0.03 * Double(z))
-                    store.append(cube)
-                }
-            }
-        }
-        for node in store {
-            sceneView.scene.rootNode.addChildNode(node)
-        }
-    }
+//    func cubeNode(_ scale: CGFloat,color: UIColor)->SCNNode{
+//        let cubeGeometry = SCNBox(width: scale, height: scale, length: scale, chamferRadius: 0.0)
+//
+//        let material = SCNMaterial()
+//        material.diffuse.contents = color
+//        cubeGeometry.materials = [material]
+//
+//        return SCNNode(geometry: cubeGeometry)
+//    }
     
+    let cubeLength = 0.1
+    let cudeDimontions = (x: 10, y: 10, z: 20)
     
-    
+    func addGrid(){
         
-    func cubeNode(_ scale: CGFloat,color: UIColor)->SCNNode{
-        let cubeGeometry = SCNBox(width: scale, height: scale, length: scale, chamferRadius: 0.0)
-        
-        let greenMaterial = SCNMaterial()
-        greenMaterial.diffuse.contents = color
-        cubeGeometry.materials = [greenMaterial]
-        
-        return SCNNode(geometry: cubeGeometry)
     }
-
 }
